7f464507106e8f959f7892ce3750fde3bb95a696
pepca <pepcape@gmail.com>
2024-03-25 00:07:06 +0100

    Intersections: base rectangle, camera pitch & yaw angles

--- src/rt004/Program.cs
+++ src/rt004/Program.cs
@@ -20,38 +20,54 @@ internal class Program
     // TODO: put anything interesting into the image.
 
     // Pilot: try to read one float number (3D camera rotation around the y axis)
-    if (args.Length > 0 &&
-        double.TryParse(args[0], NumberStyles.Float, CultureInfo.InvariantCulture, out double angle))
+    if (args.Length > 1 &&
+        double.TryParse(args[0], NumberStyles.Float, CultureInfo.InvariantCulture, out double angleX) &&
+        double.TryParse(args[1], NumberStyles.Float, CultureInfo.InvariantCulture, out double angleY))
     {
       // 3D intersection demo.
       double xMin = -1.0;
-      double xMax =  1.0;
+      double xMax = 1.0;
       double yMin = -(double)hei / wid;
       double yMax = -yMin;
       double inner = Math.Min(xMax, yMax);
 
       // AA box.
       Vector3d boxCorner = new(-inner * 0.6, -inner * 0.6, -inner * 0.6);
-      Vector3d boxSize   = new( inner * 1.2,  inner * 1.2,  inner * 1.2);
+      Vector3d boxSize = new(inner * 1.2, inner * 1.2, inner * 1.2);
 
-      // Triangle in xy plane.
-      Vector3d A = new(inner * -0.4, 0.0, 0.0);
-      Vector3d B = new(inner * 0.3, inner * -0.3, 0.0);
-      Vector3d C = new(inner * 0.1, inner * 0.3, 0.0);
+      // Triangles in the xz base plane.
+      Vector3d[] Ab = { new(inner * -0.6, inner * -0.6, inner * -0.6), new(inner * -0.6, inner * -0.6, inner * -0.6) };
+      Vector3d[] Bb = { new(inner *  0.6, inner * -0.6, inner * -0.6), new(inner *  0.6, inner * -0.6, inner *  0.6) };
+      Vector3d[] Cb = { new(inner *  0.6, inner * -0.6, inner *  0.6), new(inner * -0.6, inner * -0.6, inner *  0.6) };
+
+      // Triangle in the xy plane.
+      Vector3d A = new(inner * -0.5, 0.0,          0.0);
+      Vector3d B = new(inner *  0.4, inner * -0.4, 0.0);
+      Vector3d C = new(inner *  0.1, inner *  0.4, 0.0);
+
+      // Camera data.
+      Vector3d p1  = new(0.0, 0.0, 1.0);     // orthographic ray direction (basic orientation = z) [vector]
+      Vector3d P00 = new(xMin, yMax, -5.0);       // upper left corner of the screen [point]
+      Vector3d dx  = new((xMax - xMin) / wid, 0.0, 0.0);   // horizontal pixel step [vector]
+      Vector3d dy  = new(0.0, (yMin - yMax) / hei, 0.0);   // vertical pixel step [vector]
+
+      // Camera rotation.
+      Matrix4d m = Matrix4d.CreateRotationX(MathHelper.DegreesToRadians(angleX)) *     // pitch = "elevation" angle
+                   Matrix4d.CreateRotationY(MathHelper.DegreesToRadians(-angleY));     // yaw   = "azimuth" angle
+      P00 = Vector3d.TransformPosition(P00, m);
+      p1  = Vector3d.TransformVector(p1, m);
+      dx  = Vector3d.TransformVector(dx, m);
+      dy  = Vector3d.TransformVector(dy, m);
 
       // Image synthesis.
-      Vector3d p1  = new(0.0, 0.0, 1.0);     // orthographic ray direction (basic orientation = z)
-      Vector3d P00 = new(xMin, yMax, -5.0);
-      Vector3d dx  = new((xMax - xMin) / wid, 0.0, 0.0);
-      Vector3d dy  = new(0.0, (yMin - yMax) / hei, 0.0);
 
-      // TODO: camera rotation
-
-      float[] boxColor = { 0.0f, 0.2f, 0.2f };      // bounding box color
+      float[] boxColor  = { 0.0f, 0.2f, 0.2f };      // bounding box color
+      float[] baseColor = { 0.3f, 0.3f, 0.2f };      // base rectangle color
 
       for (int y = 0; y < hei; y++)
       for (int x = 0; x < wid; x++)
       {
+        // Single pixel [x, y]
         Vector3d P0 = P00 + x * dx + y * dy;
         float[]? color = null;
 
@@ -59,7 +75,12 @@ internal class Program
         if (MathUtil.RayBoxIntersection(P0, p1, boxCorner, boxSize, out _))
           color = boxColor;
 
-        // 2. triangle.
+        // 2. base triangles.
+        if (!double.IsInfinity(MathUtil.RayTriangleIntersection(P0, p1, Ab[0], Bb[0], Cb[0], out _)) ||
+            !double.IsInfinity(MathUtil.RayTriangleIntersection(P0, p1, Ab[1], Bb[1], Cb[1], out _)))
+          color = baseColor;
+
+        // 3. triangle.
         if (!double.IsInfinity(MathUtil.RayTriangleIntersection(P0, p1, A, B, C, out var uv)))
         {
           // Intersection exists at (1 - uv.X - uv.Y) * A + uv.X * B + uv.Y * C).
